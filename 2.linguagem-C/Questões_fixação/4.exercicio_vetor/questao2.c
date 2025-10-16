40afd97251ae45cedd3604deaeccfef98a20aaa0
Mattia Poletto <mpoletto.dev@gmail.com>
2025-10-16 18:47:36 -0300

    atualização

---
+++ 2.linguagem-C/Questões_fixação/4.exercicio_vetor/questao2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+Questão 2
+Faca um programa que receba um vetor
+com 10 valores inteiros e que indique: quantos são pares, quantos são impares,
+quantos são positivos e quantos são negativos.
+*/
+
+int main(int argc, char const *argv[]) {
+    
+    setlocale(LC_ALL,"portuguese");
+
+    int numeros[10];
+
+    for (int i=0;i<10;i++) {
+        
+
+
+    }
+
+
+
+
+
+    return 0;
+}
