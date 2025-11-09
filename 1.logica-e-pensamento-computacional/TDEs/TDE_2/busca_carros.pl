c636e17e03bde88e00bd5e8f72d32dc32f85e04d
MattiaPoletto-dev <mattia.poletto25@gmail.com>
2025-11-08 21:01:21 -0300

    projjeto prolog finalizado

---
+++ 1.logica-e-pensamento-computacional/TDEs/TDE_2/busca_carros.pl
@@ -0,0 +1,167 @@
+%-------------------------------------------------------------------------------------------------------------
+%                                                     FATOS
+%-------------------------------------------------------------------------------------------------------------
+
+% FIAT
+carro('fiat', 'argo drive 1.0'                   , 2026 , 87000  , 'hatch'  , 'manual'     , 'flex'    , 13.6 , 75  ).
+carro('fiat', 'mobi like 1.0'                    , 2026 , 69000  , 'hatch'  , 'manual'     , 'flex'    , 15.0 , 74  ).
+carro('fiat', 'fastback turbo 200'               , 2025 , 120000 , 'suv'    , 'automatico' , 'flex'    , 14.7 , 130 ).
+carro('fiat', 'fastback audace'                  , 2025 , 152000 , 'suv'    , 'automatico' , 'hibrido' , 13.0 , 130 ).
+carro('fiat', 'fastback impetus'                 , 2025 , 162000 , 'suv'    , 'automatico' , 'hibrido' , 13.0 , 130 ).
+carro('fiat', 'fastback limited edition'         , 2025 , 170000 , 'suv'    , 'automatico' , 'flex'    , 11.5 , 185 ).
+carro('fiat', 'fastback abarth'                  , 2025 , 178000 , 'suv'    , 'automatico' , 'flex'    , 11.5 , 185 ).
+carro('fiat', 'pulse turbo 200'                  , 2026 , 117000 , 'suv'    , 'automatico' , 'flex'    , 13.3 , 130 ).
+carro('fiat', 'pulse impetus'                    , 2026 , 141000 , 'suv'    , 'automatico' , 'hibrido' , 13.0 , 130 ).
+carro('fiat', 'pulse abarth'                     , 2026 , 158000 , 'suv'    , 'automatico' , 'flex'    , 11.5 , 185 ).
+carro('fiat', 'cronos drive 1.3'                 , 2025 , 90000  , 'sedan'  , 'manual'     , 'flex'    , 12.5 , 107 ).
+carro('fiat', 'cronos drive 1.0'                 , 2026 , 103000 , 'sedan'  , 'manual'     , 'flex'    , 15.0 , 75  ).
+carro('fiat', 'strada endurance'                 , 2026 , 112000 , 'picape' , 'manual'     , 'flex'    , 13.5 , 107 ).
+carro('fiat', 'strada freedom'                   , 2026 , 119000 , 'picape' , 'manual'     , 'flex'    , 12.5 , 107 ).
+carro('fiat', 'toro volcano turbodiesel 4x4 at9' , 2025 , 195000 , 'picape' , 'automatico' , 'diesel'  , 10.5 , 170 ).
+carro('fiat', 'toro endurance'                   , 2025 , 160000 , 'picape' , 'automatico' , 'flex'    , 10.5 , 180 ).
+carro('fiat', 'toro ranch'                       , 2026 , 230000 , 'picape' , 'automatico' , 'diesel'  , 10.0 , 200 ).
+carro('fiat', 'toro volcano turbo 270'           , 2025 , 185000 , 'picape' , 'automatico' , 'flex'    , 10.5 , 185 ).
+carro('fiat', 'titano ranch multijet'            , 2025 , 260000 , 'picape' , 'automatico' , 'diesel'  ,  9.5 , 180 ).
+carro('fiat', 'fiorino endurance 1.3'            , 2025 , 106000 , 'furgao' , 'manual'     , 'flex'    , 11.0 , 107 ).
+carro('fiat', 'novo ducato cargo'                , 2026 , 292000 , 'furgao' , 'manual'     , 'diesel'  , 10.5 , 140 ).
+
+% FORD
+carro('ford', 'fiesta'          , 2025 , 85000  , 'hatch'     , 'manual'     , 'flex'   , 15.0 , 120 ).
+carro('ford', 'ka'              , 2025 , 70000  , 'hatch'     , 'manual'     , 'flex'   , 14.5 , 100 ).
+carro('ford', 'territory'       , 2025 , 212000 , 'suv'       , 'automatico' , 'flex'   , 13.0 , 150 ).
+carro('ford', 'bronco sport'    , 2025 , 180000 , 'suv'       , 'automatico' , 'flex'   , 12.5 , 253 ).
+carro('ford', 'everest'         , 2025 , 220000 , 'suv'       , 'automatico' , 'flex'   , 11.0 , 250 ).
+carro('ford', 'expedition'      , 2025 , 300000 , 'suv'       , 'automatico' , 'flex'   , 10.0 , 400 ).
+carro('ford', 'eco sport'       , 2025 , 95000  , 'suv'       , 'manual'     , 'flex'   , 14.0 , 120 ).
+carro('ford', 'fusion'          , 2025 , 130000 , 'sedan'     , 'automatico' , 'flex'   , 13.5 , 190 ).
+carro('ford', 'maverick'        , 2025 , 150000 , 'picape'    , 'automatico' , 'flex'   , 12.0 , 130 ).
+carro('ford', 'ranger'          , 2025 , 170000 , 'picape'    , 'automatico' , 'flex'   , 11.5 , 185 ).
+carro('ford', 'ranger raptor'   , 2025 , 200000 , 'picape'    , 'automatico' , 'flex'   , 10.5 , 250 ).
+carro('ford', 'f-150 tremor'    , 2025 , 250000 , 'picape'    , 'automatico' , 'flex'   , 9.0  , 400 ).
+carro('ford', 'transit'         , 2025 , 180000 , 'van'       , 'manual'     , 'diesel' , 8.0  , 130 ).
+carro('ford', 'transit minibus' , 2025 , 220000 , 'van'       , 'manual'     , 'diesel' , 7.5  , 130 ).
+carro('ford', 'transit furgao'  , 2025 , 200000 , 'furgao'    , 'manual'     , 'diesel' , 8.0  , 130 ).
+carro('ford', 'mustang'         , 2025 , 300000 , 'esportivo' , 'manual'     , 'flex'   , 9.0  , 500 ).
+
+% HONDA
+carro('honda', 'city hatchback' , 2025 , 113600 , 'hatch' , 'automatico' , 'flex'     , 14.0 , 126 ).
+carro('honda', 'civic type r'   , 2025 , 429900 , 'hatch' , 'manual'     , 'gasolina' , 9.9  , 297 ).
+carro('honda', 'hr-v'           , 2025 , 152700 , 'suv'   , 'automatico' , 'flex'     , 13.3 , 126 ).
+carro('honda', 'zr-v'           , 2025 , 214500 , 'suv'   , 'automatico' , 'gasolina' , 11.1 , 161 ).
+carro('honda', 'cr-v hibrido'   , 2025 , 352900 , 'suv'   , 'automatico' , 'hibrido'  , 12.9 , 207 ).
+carro('honda', 'city sedan'     , 2025 , 115300 , 'sedan' , 'automatico' , 'flex'     , 14.0 , 126 ).
+carro('honda', 'civic hibrido'  , 2025 , 265900 , 'sedan' , 'automatico' , 'hibrido'  , 17.1 , 184 ).
+
+% NISSAN
+carro('nissan', 'kicks play 1.6'  , 2025 , 117990 , 'suv'    , 'automatico' , 'flex'     , 12.5 , 113 ).
+carro('nissan', 'kicks 1.0 turbo' , 2025 , 166990 , 'suv'    , 'automatico' , 'flex'     , 13.0 , 125 ).
+carro('nissan', 'versa'           , 2025 , 117990 , 'sedan'  , 'automatico' , 'flex'     , 13.2 , 113 ).
+carro('nissan', 'sentra'          , 2025 , 174490 , 'sedan'  , 'automatico' , 'gasolina' , 12.4 , 151 ).
+carro('nissan', 'frontier'        , 2025 , 277590 , 'picape' , 'automatico' , 'diesel'   , 10.1 , 190 ).
+
+% CHEVROLET
+carro('chevrolet', 'onix'        , 2025 , 85000  , 'hatch'     , 'automatico' , 'flex'     , 13.5 , 116 ).
+carro('chevrolet', 'tracker'     , 2025 , 119000 , 'suv'       , 'automatico' , 'flex'     , 12.5 , 133 ).
+carro('chevrolet', 'equinox'     , 2025 , 210000 , 'suv'       , 'automatico' , 'gasolina' , 10.4 , 172 ).
+carro('chevrolet', 'trailblazer' , 2025 , 370000 , 'suv'       , 'automatico' , 'diesel'   , 8.9  , 200 ).
+carro('chevrolet', 'onix plus'   , 2025 , 92000  , 'sedan'     , 'automatico' , 'flex'     , 13.3 , 116 ).
+carro('chevrolet', 'montana'     , 2025 , 145000 , 'picape'    , 'automatico' , 'flex'     , 11.7 , 133 ).
+carro('chevrolet', 's10'         , 2025 , 270000 , 'picape'    , 'automatico' , 'diesel'   , 9.2  , 200 ).
+carro('chevrolet', 'spin'        , 2025 , 115000 , 'minivan'   , 'automatico' , 'flex'     , 11.8 , 110 ).
+carro('chevrolet', 'camaro'      , 2025 , 520000 , 'esportivo' , 'automatico' , 'gasolina' , 6.5  , 461 ).
+
+% MITSUBISHI
+carro('mitsubishi', 'outlander'       , 2025 , 270000 , 'suv'    , 'automatico' , 'hibrido' , 20.0 , 240 ).
+carro('mitsubishi', 'outlander sport' , 2025 , 160000 , 'suv'    , 'automatico' , 'flex'    , 11.2 , 170 ).
+carro('mitsubishi', 'eclipse cross'   , 2025 , 175000 , 'suv'    , 'automatico' , 'flex'    , 10.8 , 165 ).
+carro('mitsubishi', 'pajero sport'    , 2025 , 340000 , 'suv'    , 'automatico' , 'diesel'  , 8.5  , 190 ).
+carro('mitsubishi', 'l200 triton'     , 2025 , 260000 , 'picape' , 'automatico' , 'diesel'  , 9.5  , 190 ).
+
+% RENAULT
+carro('renault', 'kwid'                 , 2025 , 77240  , 'hatch'  , 'manual'     , 'flex'     , 15.3 , 71  ).
+carro('renault', 'kwid e-tech'          , 2026 , 149000 , 'hatch'  , 'automatico' , 'eletrico' , 0.0  , 65  ).
+carro('renault', 'sandero'              , 2024 , 83190  , 'hatch'  , 'manual'     , 'flex'     , 11.5 , 82  ).
+carro('renault', 'stepway'              , 2025 , 86990  , 'hatch'  , 'manual'     , 'flex'     , 11.5 , 82  ).
+carro('renault', 'duster 1.6 manual'    , 2026 , 141990 , 'suv'    , 'manual'     , 'flex'     , 11.2 , 120 ).
+carro('renault', 'duster 1.3 turbo cvt' , 2026 , 176990 , 'suv'    , 'cvt'        , 'flex'     , 11.5 , 170 ).
+carro('renault', 'kardian 1.0 manual'   , 2026 , 113690 , 'suv'    , 'manual'     , 'flex'     , 12.7 , 125 ).
+carro('renault', 'kardian 1.0 dct'      , 2026 , 149990 , 'suv'    , 'dct'        , 'flex'     , 12.8 , 125 ).
+carro('renault', 'boreal'               , 2026 , 179990 , 'suv'    , 'dct'        , 'flex'     , 12.0 , 170 ).
+carro('renault', 'captur'               , 2024 , 142990 , 'suv'    , 'cvt'        , 'flex'     , 10.8 , 170 ).
+carro('renault', 'megane e-tech'        , 2025 , 279900 , 'suv'    , 'automatico' , 'eletrico' , 0.0  , 220 ).
+carro('renault', 'logan'                , 2025 , 97500  , 'sedan'  , 'manual'     , 'flex'     , 13.6 , 82  ).
+carro('renault', 'oroch 1.6'            , 2025 , 126990 , 'picape' , 'manual'     , 'flex'     , 11.1 , 120 ).
+carro('renault', 'oroch 1.3 turbo'      , 2025 , 156970 , 'picape' , 'cvt'        , 'flex'     , 11.3 , 170 ).
+carro('renault', 'kangoo'               , 2026 , 130690 , 'van'    , 'manual'     , 'flex'     , 11.3 , 115 ).
+carro('renault', 'master'               , 2026 , 248710 , 'van'    , 'manual'     , 'diesel'   , 8.0  , 136 ).
+
+% HYUNDAI
+carro('hyundai', 'hb20'     , 2025 , 93310  , 'hatch' , 'manual'     , 'flex'     , 13.4 , 80  ).
+carro('hyundai', 'creta'    , 2026 , 151290 , 'suv'   , 'automatico' , 'flex'     , 12.0 , 120 ).
+carro('hyundai', 'tucson'   , 2025 , 199490 , 'suv'   , 'automatico' , 'gasolina' , 10.6 , 177 ).
+carro('hyundai', 'kona'     , 2025 , 214990 , 'suv'   , 'dct'        , 'hibrido'  , 18.4 , 141 ).
+carro('hyundai', 'palisade' , 2026 , 479990 , 'suv'   , 'automatico' , 'gasolina' , 7.3  , 295 ).
+carro('hyundai', 'hb20s'    , 2026 , 103090 , 'sedan' , 'manual'     , 'flex'     , 12.5 , 80  ).
+
+%-------------------------------------------------------------------------------------------------------------
+%                                                     Regras
+%-------------------------------------------------------------------------------------------------------------
+
+% Guia inicial
+guia :-
+    writeln("COMANDOS DISPONIVEIS:"), nl,
+    writeln("Para saber quais filtros sao possiveis, use:"),
+    writeln("  --> filtros_possiveis."), nl,
+    writeln("Para filtrar os carros, use:"),
+    writeln("  --> filtro('Marca', Ano, PrecoMin, PrecoMax, 'Tipo', 'Cambio', 'Combustivel', ConsumoMax, PotMin, PotMax)."),
+    writeln("     obs: use _ para os parametros que nao te interessam."), nl, 
+    writeln("Para buscar as informacoes de um determinado carro, use:"),
+    writeln("  --> carro_info('nome do carro')."), nl, nl.
+
+
+% Filtros possíveis
+filtros_possiveis :-
+    writeln("Marcas        : 'fiat'  'ford'  'honda'  'nissan'  'chevrolet'  'mitsubishi'  'renault'  'hyundai'"),
+    writeln("Tipo de carro : 'hatch'  'suv'  'sedan'  'picape'  'minivan'  'van'  'furgao'  'esportivo'"),
+    writeln("Cambio        : 'manual'  'automatico'  'cvt'  'dct'"),
+    writeln("Combustivel   : 'gasolina'  'flex'  'diesel'  'eletrico'  'hibrido'"),
+    writeln("Os outros parametros sao aceitos apenas numeros."), nl.
+
+
+% Filtro de carros
+:- dynamic visto/1.
+filtro(Marca, Ano, PrecoMin, PrecoMax, Tipo, Transmissao, Combustivel, ConsumoMax, PotMin, PotMax) :-
+    carro(MarcaDB, Nome, AnoDB, PrecoDB, TipoDB, TransmissaoDB, CombustivelDB, ConsumoDB, PotenciaDB),
+
+    % filtros opcionais
+    ( var(Marca)       ; Marca = MarcaDB ),
+    ( var(Ano)         ; Ano = AnoDB ),
+    ( nonvar(PrecoMin) -> PrecoDB >= PrecoMin ; true ),
+    ( nonvar(PrecoMax) -> PrecoDB =< PrecoMax ; true ),
+    ( var(Tipo)        ; Tipo = TipoDB ),
+    ( var(Transmissao) ; Transmissao = TransmissaoDB ),
+    ( var(Combustivel) ; Combustivel = CombustivelDB ),
+    ( nonvar(ConsumoMax) -> ConsumoDB =< ConsumoMax ; true ),
+    ( nonvar(PotMin) -> PotenciaDB >= PotMin ; true ),
+    ( nonvar(PotMax) -> PotenciaDB =< PotMax ; true ),
+
+    % imprime apenas uma vez
+    ( \+ visto(Nome) -> writeln(Nome), assert(visto(Nome)) ; true ),
+    fail.
+
+% limpa a lista de carros vistos e encerra a execução
+filtro(_, _, _, _, _, _, _, _, _, _) :-
+    retractall(visto(_)).
+
+
+% Informações de um carro específico
+carro_info(NomeBuscado) :-
+    carro(Marca, NomeBuscado, Ano, Preco, Tipo, Cambio, Combustivel, Consumo, Potencia),
+    write('Marca          : '), writeln(Marca),
+    write('Ano            : '), writeln(Ano),
+    write('Preco medio    : '), writeln(Preco),
+    write('Tipo           : '), writeln(Tipo),
+    write('Combustivel    : '), writeln(Combustivel),
+    write('Cambio         : '), writeln(Cambio),
+    write('Consumo (km/l) : '), writeln(Consumo),
+    write('Potencia (cv)  : '), writeln(Potencia), nl.
